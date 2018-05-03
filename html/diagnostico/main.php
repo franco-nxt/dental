4f2287609093a489e7d258d2a365a91e709d2d7c
franco-nxt <franco.n.quintana@gmail.com>
2018-05-03 16:28:10 -0300

    tratamiento->inicio ahora es fecha_hora_inicio Treatment->url ahora ya no es necesario agregar el subpath 'diagnostico'

--- html/diagnostico/main.php
+++ html/diagnostico/main.php
@@ -14,13 +14,13 @@ $treatments = (Array) $Patient->old_treatments();
 </div>
 <div class="container p5">
 	<div class="bar-bordered">
-		<span><?= $Treatment->inicio ?> - <?= $Treatment->estado ?> - <?= $Treatment->tecnica ?> - <?= $Treatment->descripcion ?></span>
+		<span><?= $Treatment->fecha_hora_inicio ?> - <?= $Treatment->estado ?> - <?= $Treatment->tecnica ?> - <?= $Treatment->descripcion ?></span>
 	</div>
 	<ul class="p0">
-		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('historia') ?>" class="btn btn-primary btn-block"><strong>HISTORIA MEDICA Y DENTAL</strong></a></li>
-		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('examen') ?>" class="btn btn-primary btn-block"><strong>EXAMEN CLINICO Y BUCAL</strong></a></li>
-		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('completo') ?>" class="btn btn-primary btn-block"><strong>DIAGNÓSTICO</strong></a></li>
-		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('resumen') ?>" class="btn btn-primary btn-block"><strong>RESUMEN DIAGNÓSTICO</strong></a></li>
+		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('diagnostico/historia') ?>" class="btn btn-primary btn-block"><strong>HISTORIA MEDICA Y DENTAL</strong></a></li>
+		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('diagnostico/examen') ?>" class="btn btn-primary btn-block"><strong>EXAMEN CLINICO Y BUCAL</strong></a></li>
+		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('diagnostico/completo') ?>" class="btn btn-primary btn-block"><strong>DIAGNÓSTICO</strong></a></li>
+		<li class="p5 sm-4 center"><a href="<?= $Treatment->url('diagnostico/resumen') ?>" class="btn btn-primary btn-block"><strong>RESUMEN DIAGNÓSTICO</strong></a></li>
 	</ul>
 	<?php if (!empty($treatments)): ?>
 		<div class="p5 txt-center">
@@ -29,13 +29,13 @@ $treatments = (Array) $Patient->old_treatments();
 		<div id="old-treatments" style="display:none">
 			<?php foreach ($treatments as $treatment): ?>
 				<div class="bar-bordered">
-					<span><?= $treatment->inicio ?> - <?= $treatment->estado ?> - <?= $treatment->tecnica ?> - <?= $treatment->descripcion ?></span>
+					<span><?= $Treatment->fecha_hora_inicio ?> - <?= $treatment->estado ?> - <?= $treatment->tecnica ?> - <?= $treatment->descripcion ?></span>
 				</div>
 				<ul class="p0">
-					<li class="p5 sm-4 center"><a href="<?= $treatment->url('historia') ?>" class="btn btn-primary btn-block"><strong>HISTORIA MEDICA Y DENTAL</strong></a></li>
-					<li class="p5 sm-4 center"><a href="<?= $treatment->url('examen') ?>" class="btn btn-primary btn-block"><strong>EXAMEN CLINICO Y BUCAL</strong></a></li>
-					<li class="p5 sm-4 center"><a href="<?= $treatment->url('completo') ?>" class="btn btn-primary btn-block"><strong>DIAGNÓSTICO</strong></a></li>
-					<li class="p5 sm-4 center"><a href="<?= $treatment->url('resumen') ?>" class="btn btn-primary btn-block"><strong>RESUMEN DIAGNÓSTICO</strong></a></li>
+					<li class="p5 sm-4 center"><a href="<?= $treatment->url('diagnostico/historia') ?>" class="btn btn-primary btn-block"><strong>HISTORIA MEDICA Y DENTAL</strong></a></li>
+					<li class="p5 sm-4 center"><a href="<?= $treatment->url('diagnostico/examen') ?>" class="btn btn-primary btn-block"><strong>EXAMEN CLINICO Y BUCAL</strong></a></li>
+					<li class="p5 sm-4 center"><a href="<?= $treatment->url('diagnostico/completo') ?>" class="btn btn-primary btn-block"><strong>DIAGNÓSTICO</strong></a></li>
+					<li class="p5 sm-4 center"><a href="<?= $treatment->url('diagnostico/resumen') ?>" class="btn btn-primary btn-block"><strong>RESUMEN DIAGNÓSTICO</strong></a></li>
 				</ul>
 			<?php endforeach ?>
 		</div>

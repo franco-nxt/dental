5c6efcfb3ce47301702d5b6678174588f8aff58f
franco-next <franco.n.quintana@gmail.com>
2018-05-07 17:15:35 -0300

    Refact en nuevo method. Catcheo de exceptions en el constructor del controlador.

--- actions/tratamiento.php
+++ actions/tratamiento.php
@@ -3,9 +3,24 @@
 class Page extends Controller{
 
     public function __construct() {
-        parent::__construct(
-            array('tratamiento/editar/[:encode]', 'editar'),
-            array('tratamiento/nuevo/[:encode]', 'nuevo'));
+        try{
+            parent::__construct(
+                array('tratamiento/editar/[:encode]', 'editar'),
+                array('tratamiento/nuevo/[:encode]', 'nuevo')
+            );
+        } 
+        catch (PatientException $e) {
+            add_error_flash($e->getMessage());
+        }
+        catch (TreatmentException $e) {
+            add_error_flash($e->getMessage());
+        }
+        catch (Exception $e) {
+            add_error_flash('NO SE PUEDE PROCESAR LA ORDEN.');
+        }
+        finally{
+            redirect_exit();
+        }
     }
     
     public function main($encode) {
@@ -18,14 +33,7 @@ class Page extends Controller{
         $Patient = decode_patient($encode);
         // OBTENGO EL TRATAMIENTO
         $Treatment = $Patient->get_treatment(get_from_encode($encode, TRATAMIENTO));
-
         $FormValidator = $this->validate_form();
-        
-        $FormValidator->add_rule("duracion", "num&lessthan=99");
-        $FormValidator->add_rule("tecnica", "rgx=#1|2#");
-        $FormValidator->add_rule("presupuesto", "alnum_s");
-        $FormValidator->add_rule("descripcion", "maxlen=200");
-
         // VALIDO EL FORM
         if($FormValidator->validate()){
             $form_data = $FormValidator->input; // POST DATA
@@ -43,33 +51,22 @@ class Page extends Controller{
 
     public function nuevo($encode) 
     {
-        $decrypt_params = decrypt_params($encode);
-        // SI NO ESTAN ESTOS DATOS NO AVANZA
-        if (!isset($decrypt_params[PACIENTE])){
-            add_error_flash("NO SE PUDO EDITAR EL TRATAMIENTO.");
-            redirect_exit();
-        }
-
-        $Patient = get_patient($decrypt_params[PACIENTE]);
-
+        // OBTENGO EL PACIENTE DESDE EL ID ENCODEADO
+        $Patient = decode_patient($encode);
         $FormValidator = $this->validate_form();
-        
-        $FormValidator->add_rule("duracion", "num&lessthan=99");
-        $FormValidator->add_rule("tecnica", "rgx=#1|2#");
-        $FormValidator->add_rule("presupuesto", "alnum_s");
-        $FormValidator->add_rule("descripcion", "maxlen=200");
-
         // VALIDO EL FORM
         if($FormValidator->validate()){
-            $form_data = $FormValidator->input; // POST DATA
-            $Treatment = $Patient->get_treatment($form_data);
-
+            // POST DATA
+            $form_data = $FormValidator->input; 
+            // CON ESA INFO CREO EL TRATAMIENTO
+            $Treatment = $Patient->create_treatment($form_data);
             // MENSAJE PARA EL FRONT
             add_msg_flash('SE CREO UN NUEVO TRATAMIENTO CON EXITO.');
         }
         else{
             add_error_flash(implode('<br/>', $FormValidator->errors));
         }
+
         redirect_exit($Patient->url());
     }
 

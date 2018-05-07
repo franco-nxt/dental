32705470e8daf192ee669574c6ef54ed77ff3549
franco-next <franco.n.quintana@gmail.com>
2018-05-07 17:10:07 -0300

    Destruyo la session en el constructor.

--- modules/logout.php
+++ modules/logout.php
@@ -3,13 +3,6 @@
 class Page extends Controller{
 
 	public function __construct() {
-		// _global('navbar-title', 'PACIENTES');
-
-		$this->main();
-	}
-	
-	public function main() {
-		echo 'asdasdasdas';
 		load_class('Session')->destroy();
 		redirect_exit();
 	}

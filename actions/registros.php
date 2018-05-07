e7be0a6fd3a38fbf797c2b09f369f0d6d59ff365
franco-next <franco.n.quintana@gmail.com>
2018-05-07 17:18:53 -0300

    Refact de todo el controller y catcheo las expections en el construct.

--- actions/registros.php
+++ actions/registros.php
@@ -3,69 +3,67 @@
 class Page extends Controller{
 
 	public function __construct() {
-		parent::__construct(
-			array('registros/nuevo/[:encode]', 'nuevo'),
-			array('registros/editar/[:encode]', 'editar'));
-	}
-
-	public function nuevo($id)
-	{
-		$decrypt_params = decrypt_params($id);
-		// SI NO ESTAN ESTOS DATOS NO AVANZA
-		if (isset($decrypt_params[PACIENTE], $decrypt_params[TRATAMIENTO])){
-			$Patient = get_patient($decrypt_params[PACIENTE]);
-			$Treatment = $Patient->get_treatment($decrypt_params[TRATAMIENTO]);
-	
-			$FormValidator = $this->validate_form();
-
-			if($FormValidator->validate()){
-				$Register = $Treatment->create_register($FormValidator->input);
-
-				// MENSAJE PARA EL FRONT
-				add_msg_flash('REGISTRO CREADO CON EXITO.');
-				redirect_exit($Register->url('ver'));
-			}
-			else{
-				$error = implode('<br/>', $FormValidator->errors);
-				add_error_flash($error);
-				redirect_exit(URL_ROOT . "/registros/nuevo/" . $Treatment->url);
-			}
+		try {
+			parent::__construct(
+				array('registros/nuevo/[:encode]', 'nuevo'),
+				array('registros/editar/[:encode]', 'editar')
+			);
 		}
-		else{
-			dump($decrypt_params);
-			// add_msg_flash("NO SE PUEDE PROCESAR LA ORDEN.");
-			// redirect_exit();
+		catch (PatientException $e) {
+			add_error_flash($e->getMessage());
 		}
-	}
-
-	public function editar($id)
-	{
-		$decrypt_params = decrypt_params($id);
-		// SI NO ESTAN ESTOS DATOS NO AVANZA
-		if (isset($decrypt_params[PACIENTE], $decrypt_params[TRATAMIENTO], $decrypt_params[REGISTRO])){
-			$Patient = get_patient($decrypt_params[PACIENTE]);
-			$Treatment = $Patient->get_treatment($decrypt_params[TRATAMIENTO]);
-			$Register = $Treatment->get_register($decrypt_params[REGISTRO]);
-	
-			$FormValidator = $this->validate_form();
-
-			if($FormValidator->validate()){
-
-				$Register->update($FormValidator->input);
-
-				// MENSAJE PARA EL FRONT
-				add_msg_flash('SE GUARDARON LOS CAMBIOS CON EXITO.');
-				redirect_exit($Register->url('ver'));
-			}
-			else{
-				$error = implode('<br/>', $FormValidator->errors);
-				add_error_flash($error);
-				redirect_exit($Register->url('editar'));
-			}
+		catch (TreatmentException $e) {
+			add_error_flash($e->getMessage());
 		}
-		else{
-			add_msg_flash("NO SE PUEDE PROCESAR LA ORDEN.");
+		catch (RegisterException $e) {
+			add_error_flash($e->getMessage());
+		}
+		catch (Exception $e) {
+			add_error_flash('NO SE PUEDE PROCESAR LA ORDEN.');
+		}
+		finally{
 			redirect_exit();
+		}
+	}
+
+	public function nuevo($encode)
+	{
+		$Patient = decode_patient($encode);
+		$Treatment = $Patient->get_treatment(get_from_encode($encode, TRATAMIENTO));
+		
+		$FormValidator = $this->validate_form();
+
+		if($FormValidator->validate()){
+			$Register = $Treatment->create_register($FormValidator->input);
+			// MENSAJE PARA EL FRONT
+			add_msg_flash('REGISTRO CREADO CON EXITO.');
+			redirect_exit($Register->url('ver'));
+		}
+		else{
+			$error = implode('<br/>', $FormValidator->errors);
+			add_error_flash($error);
+			redirect_exit(URL_ROOT . "/registros/nuevo/" . $Treatment->url);
+		}
+	}
+
+	public function editar($encode)
+	{
+		$Patient = decode_patient($encode);
+		$Treatment = $Patient->get_treatment(get_from_encode($encode, TRATAMIENTO));
+		$Register = $Treatment->get_register(get_from_encode($encode, REGISTRO));
+	
+		$FormValidator = $this->validate_form();
+
+		if($FormValidator->validate()){
+			$Register->update($FormValidator->input);
+			// MENSAJE PARA EL FRONT
+			add_msg_flash('SE GUARDARON LOS CAMBIOS CON EXITO.');
+			redirect_exit($Register->url('ver'));
+		}
+		else{
+			$error = implode('<br/>', $FormValidator->errors);
+			add_error_flash($error);
+			redirect_exit($Register->url('editar'));
 		}
 	}
 

0eb78bef706c41199cb1c720e9c2941eb482ac64
franco-next <franco.n.quintana@gmail.com>
2018-05-07 14:26:02 -0300

    Clases

---
+++ libs/classes.lib.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "classes/class.Mysql.php";
+require_once "classes/Session.php";
+require_once "classes/Controller.php";
+
+require_once "classes/Controller.php";
+
+require_once "classes/Dental/Dental.php";
+require_once "classes/Dental/Patient.php";
+require_once "classes/Dental/Treatment.php";
+require_once "classes/Dental/Photo.php";
+require_once "classes/Dental/Radiographie.php";
+require_once "classes/Dental/Cephalometry.php";
+require_once "classes/Dental/Register.php";
+require_once "classes/Dental/Payment.php";
+require_once "classes/Dental/History.php";
+require_once "classes/Dental/Exam.php";
+require_once "classes/Dental/Resume.php";
+require_once "classes/Dental/Diagnostic.php";
+require_once "classes/Dental/Odontogram.php";
+
+require_once "classes/Exceptions/DentalException.php";
+require_once "classes/Exceptions/PatientException.php";
+require_once "classes/Exceptions/TreatmentException.php";
+require_once "classes/Exceptions/PhotoException.php";
+require_once "classes/Exceptions/RadiographieException.php";
+require_once "classes/Exceptions/CephalometryException.php";
+require_once "classes/Exceptions/RegisterException.php";
+require_once "classes/Exceptions/PaymentException.php";
+require_once "classes/Exceptions/HistoryException.php";
+require_once "classes/Exceptions/ExamException.php";
+require_once "classes/Exceptions/ResumeException.php";
+require_once "classes/Exceptions/DiagnosticException.php";
+require_once "classes/Exceptions/OdontogramException.php";

05c8442861d8e1b4e21975085704eb563ec474fc
franco-next <franco.n.quintana@gmail.com>
2018-05-22 15:56:30 -0300

    Validaciones js para los formularios de diagnostico.

--- js/src/components/forms.js
+++ js/src/components/forms.js
@@ -1,78 +1,151 @@
-window.seton = function(){
-	for (var i = 0; i < arguments.length; i++){
-		var matches = document.querySelectorAll(arguments[i]);
+import swal from 'sweetalert';
+import {isTextBox} from './utils';
+require('jquery-nice-select');
 
-		for (var c = 0; c < matches.length; c++){
-			if (matches[c]){
-				matches[c].disabled = false;
-				matches[c].focus();
-				removeClass(matches[c], 'disabled');
-				if(matches[c].labels && matches[c].labels[0]){
-					removeClass(matches[c].labels[0], 'disabled');
-					addClass(matches[c].labels[0], 'active');
-				}
-			}
-		}
-	}
+window.seton = function(){
+	// for (var i = 0; i < arguments.length; i++){
+	// 	var matches = document.querySelectorAll(arguments[i]);
+
+	// 	for (var c = 0; c < matches.length; c++){
+	// 		if (matches[c]){
+	// 			matches[c].disabled = false;
+	// 			matches[c].focus();
+	// 			removeClass(matches[c], 'disabled');
+	// 			if(matches[c].labels && matches[c].labels[0]){
+	// 				removeClass(matches[c].labels[0], 'disabled');
+	// 				addClass(matches[c].labels[0], 'active');
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
 
 window.setoff = function(){
-	for (var i = 0; i < arguments.length; i++){
-		var matches = document.querySelectorAll(arguments[i]);
+	// for (var i = 0; i < arguments.length; i++){
+	// 	var matches = document.querySelectorAll(arguments[i]);
 
-		for (var c = 0; c < matches.length; c++){
-			if(matches[c]){
-				matches[c].checked = false;
-				matches[c].disabled = true;
-				isTextBox(matches[c]) && (matches[c].value = '');
-				addClass(matches[c], 'disabled');
-				if(matches[c].labels && matches[c].labels[0]){
-					addClass(matches[c].labels[0], 'disabled');
-					removeClass(matches[c].labels[0], 'active');
-				}
+	// 	for (var c = 0; c < matches.length; c++){
+	// 		if(matches[c]){
+	// 			matches[c].checked = false;
+	// 			matches[c].disabled = true;
+	// 			isTextBox(matches[c]) && (matches[c].value = '');
+	// 			addClass(matches[c], 'disabled');
+	// 			if(matches[c].labels && matches[c].labels[0]){
+	// 				addClass(matches[c].labels[0], 'disabled');
+	// 				removeClass(matches[c].labels[0], 'active');
+	// 			}
+	// 		}
+	// 	}
+	// }
+}
+
+
+
+// $("[data-discheck]").on('change', function(){
+// 	$(this.getAttribute('data-discheck')).each(function (i, elm) {
+// 		elm.setAttribute('checked', false);
+// 		elm.checked = false;
+// 	});
+// });
+
+window.check_csv = function(sender) {
+	var validExts = new Array(".csv");
+	var fileExt = sender.value;
+	fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
+
+	if (validExts.indexOf(fileExt) < 0) {
+		swal("El archivo no es una formato valido.");
+		return false;
+	}
+	else{
+		sender.form.submit();
+	}
+}
+
+window.change_si = function(_){
+	var actual = document.querySelector(_.actual);
+	var pasado = document.querySelector(_.pasado);
+
+	if(this.checked){
+		document.querySelector(_.no).checked = false;
+		
+		[actual, pasado].forEach(function(a){
+			a.disabled = false;
+			if (a.labels.length){
+				a.labels[0].className = a.labels[0].className.replace(/\bdisabled\b/g, ' ');
 			}
-		}
+		});
+	}
+	else{
+		[actual, pasado].forEach(function(a){
+			a.disabled = true;
+			a.checked = false;
+			if (a.labels.length && a.labels[0].className.split(' ').indexOf('disabled') == -1) {
+				a.labels[0].className += ' disabled';
+			}
+		});
 	}
 }
 
-function addClass(el, className)
-{
-	if (el.classList)
-		el.classList.add(className)
-	else if (!hasClass(el, className))
-		el.className += " " + className;
-}
+window.change_no = function(_){
+	var actual = document.querySelector(_.actual);
+	var pasado = document.querySelector(_.pasado);
 
-function removeClass(el, className)
-{
-	if (el.classList)
-		el.classList.remove(className)
-	else if (hasClass(el, className))
-	{
-		var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-		el.className = el.className.replace(reg, ' ');
+	$(_.hasta).prop('disabled', 'disabled').niceSelect('update');
+
+	if(this.checked){
+		document.querySelector(_.si).checked = false;
+	
+		[actual, pasado].forEach(function(a){
+			a.disabled = true;
+			a.checked = false;
+			if (a.labels.length && a.labels[0].className.split(' ').indexOf('disabled') == -1) {
+				a.labels[0].className += ' disabled';
+			}
+		});
 	}
 }
-function hasClass(el, className)
-{
-	if (el.classList)
-		return el.classList.contains(className);
-	return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+
+window.change_actual = function(_){
+	if(this.checked){
+		document.querySelector(_.pasado).checked = false;
+		$(_.hasta).prop('disabled', 'disabled').niceSelect('update');
+	}
 }
 
-function isTextBox(element) {
-    var tagName = element.tagName.toLowerCase();
-    if (tagName === 'textarea') return true;
-    if (tagName !== 'input') return false;
-    var type = element.getAttribute('type').toLowerCase(),
-        // if any of these input types is not supported by a browser, it will behave as input type text.
-        inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
-    return inputTypes.indexOf(type) >= 0;
+window.change_pasado = function(_){
+	if(this.checked){
+		document.querySelector(_.actual).checked = false;
+		$(_.hasta).prop('disabled', false).niceSelect('update');
+	}
 }
 
-$("[data-discheck]").on('change', function(){
-	$(this.getAttribute('data-discheck')).each(function (i, elm) {
-		elm.setAttribute('checked', false);
-		elm.checked = false;
+$('.field[data-change]').on('change', 'input', function(e){
+	console.log(this.getAttribute('data-change'));
+});
+
+$('input[data-discheck]').on('change', function(){
+	$(this.getAttribute('data-discheck')).each(function(){
+		this.checked = false;
 	});
+});
+
+$('.field[data-check-one]').on('change', 'input', function(e){
+	$(e.delegateTarget).find('input').not(this).each(function(a,b){
+		b.checked = false;
+	})
+});
+
+$('input[data-enable-text]').on('change', function(e){
+	if(this.checked){
+		var input = $(document.querySelector(this.getAttribute('data-enable-text'))).prop('disabled', false).focus();
+		$(input[0].labels[0]).removeClass('disabled')
+	}
+});
+
+$('input[data-disable-text]').on('change', function(e){
+	if(this.checked){
+		var input = $(document.querySelector(this.getAttribute('data-disable-text'))).prop('disabled', 'disabled').val('');
+		$(input[0].labels[0]).addClass('disabled')
+	}
 });
